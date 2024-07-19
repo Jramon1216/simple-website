61257ff731885f94710bf0b85c7c0154ab7cf44c
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-19 18:56:38 -0400

    *

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://simplesitebackend.ddns.net',
+    baseURL: 'https://simplesitebackend.ddns.net',
 });
 
 export default api;
