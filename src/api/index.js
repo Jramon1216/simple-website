63918f8f4e17e59bd44d50e7c7801577b0d1fecc
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-19 17:15:26 -0400

    *

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ec2-3-142-240-212.us-east-2.compute.amazonaws.com:80',
+    baseURL: 'http://ec2-3-142-240-212.us-east-2.compute.amazonaws.com:80',
 });
 
 export default api;
