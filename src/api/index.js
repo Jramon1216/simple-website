c9899754abd00960c4104e546e5339362b10c5b8
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-19 15:39:15 -0400

    - corrected http typo

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://ec2-3-142-240-212.us-east-2.compute.amazonaws.com:80',
+    baseURL: 'https://ec2-3-142-240-212.us-east-2.compute.amazonaws.com:80',
 });
 
 export default api;
